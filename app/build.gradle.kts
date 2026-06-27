71f9d8f6af3fbfc352fbfc84cc394829f1b45abe
Leaf <itzsimplyleaf345@gmail.com>
2026-06-27 16:18:41 +0200

    removed implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,11 +48,7 @@ android {
         buildConfig = true
     }
 
-    packaging {
-        jniLibs {
-            useLegacyPackaging = true
-        }
-    }
+
 }
 
 dependencies {
@@ -68,7 +64,6 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.media3.exoplayer)
-    implementation(libs.androidx.media3.exoplayer.ffmpeg)
     implementation(libs.androidx.media3.session)
     implementation(libs.androidx.media3.ui)
     implementation(libs.coil.compose)
